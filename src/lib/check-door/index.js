5c6b13007ed2ffb5392323af8b003cf646129dd4
cory-p-oncota <coryparent@oncota.com>
2015-07-26 19:09:22 +0000

    open port

--- src/lib/check-door/index.js
+++ src/lib/check-door/index.js
@@ -5,8 +5,10 @@ var gpio = require('pi-gpio');
 var CONTACT_PIN = process.env.DOOR_READ_PIN;
 
 module.exports = function (callback, req) {
-  gpio.read(CONTACT_PIN, function (err, state) {
-    if (err) throw err;
-    callback(state);
+  gpio.open(CONTACT_PIN, 'input', function (err, res) {
+    gpio.read(CONTACT_PIN, function (err, state) {
+      if (err) throw err;
+      callback(state);
+    });
   });
 };
